e4fe9c997aa601a6558800cd58411abac0f467ca
Greg McGuirk and Jon Bardin <pair+greg+jon@mavenlink.com>
2015-02-27 14:16:49 -0800

    lazy start nodejs server, in a thread safe manner

--- lib/runtime.rb
+++ lib/runtime.rb
@@ -17,7 +17,7 @@ module Alaska
         path
       end
 
-      run_server
+      @semaphore = Mutex.new
     end
 
     def name
@@ -37,7 +37,10 @@ module Alaska
       Alaska::Context
     end
 
+    #NOTE: this should be thread-safe
     def provision_socket
+      ensure_startup unless @pid
+
       wait_socket = nil
       checks = 0
       max_retries = 6
@@ -63,15 +66,19 @@ module Alaska
 
     private
 
-    def run_server
-      alaska_js_path = File.join(File.dirname(File.expand_path(__FILE__)), '../alaska.js')
-      command_options = [alaska_js_path, "--debug #{!!@debug}"] # --other --command-line --options --go --here
+    def ensure_startup
+      @semaphore.synchronize {
+        return if @pid
 
-      @pid = Process.spawn({"PORT" => @port.to_s}, @nodejs_cmd, *command_options, {:err => :out})
+        alaska_js_path = File.join(File.dirname(File.expand_path(__FILE__)), '../alaska.js')
+        command_options = [alaska_js_path, "--debug #{!!@debug}"] # --other --command-line --options --go --here
 
-      at_exit do
-        ensure_shutdown
-      end
+        @pid = Process.spawn({"PORT" => @port.to_s}, @nodejs_cmd, *command_options, {:err => :out})
+
+        at_exit do
+          ensure_shutdown
+        end
+      }
     end
 
     def ensure_shutdown
